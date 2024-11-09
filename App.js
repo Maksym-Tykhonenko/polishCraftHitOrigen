51bdc66e12a24e62c4d80365bb33d021bd00f4b6
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2024-11-09 11:49:58 +0200

    save

--- App.js
+++ App.js
@@ -14,6 +14,14 @@ import LegendsScreen from './src/screens/LegendsScreen';
 import PlacesScreen from './src/screens/PlacesScreen';
 import MapScreen from './src/screens/MapScreen';
 import PolishCraftHitOrigenProdactScreen from './src/screens/PolishCraftHitOrigenProdactScreen';
+/////////////////////////
+import ReactNativeIdfaAaid, {
+  AdvertisingInfoResponse,
+} from '@sparkfabrik/react-native-idfa-aaid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppleAdsAttribution from '@hexigames/react-native-apple-ads-attribution';
+import DeviceInfo from 'react-native-device-info';
+import appsFlyer from 'react-native-appsflyer';
 
 enableScreens();
 
@@ -22,13 +30,233 @@ const Stack = createStackNavigator();
 const App = () => {
   const [route, setRoute] = useState(true);
   //console.log('route==>', route)
+  const [idfa, setIdfa] = useState();
+  console.log('idfa==>', idfa);
+  const [appsUid, setAppsUid] = useState(null);
+  const [sab1, setSab1] = useState();
+  const [pid, setPid] = useState();
+  console.log('appsUid==>', appsUid);
+  console.log('sab1==>', sab1);
+  console.log('pid==>', pid);
+  const [adServicesToken, setAdServicesToken] = useState(null);
+  //console.log('adServicesToken', adServicesToken);
+  const [adServicesAtribution, setAdServicesAtribution] = useState(null);
+  const [adServicesKeywordId, setAdServicesKeywordId] = useState(null);
+  ////////
+  const [customerUserId, setCustomerUserId] = useState(null);
+  //console.log('customerUserID==>', customerUserId);
+  const [idfv, setIdfv] = useState();
+  //console.log('idfv==>', idfv);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [
+    idfa,
+    appsUid,
+    sab1,
+    pid,
+    adServicesToken,
+    adServicesAtribution,
+    adServicesKeywordId,
+    customerUserId,
+    idfv,
+  ]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+        appsUid,
+        sab1,
+        pid,
+        adServicesToken,
+        adServicesAtribution,
+        adServicesKeywordId,
+        customerUserId,
+        idfv,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('App', jsonData);
+      //console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      //console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('App');
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('Дані дістаються в AsyncStorage');
+        console.log('parsedData in App==>', parsedData);
+        setIdfa(parsedData.idfa);
+        setAppsUid(parsedData.appsUid);
+        setSab1(parsedData.sab1);
+        setPid(parsedData.pid);
+        setAdServicesToken(parsedData.adServicesToken);
+        setAdServicesAtribution(parsedData.adServicesAtribution);
+        setAdServicesKeywordId(parsedData.adServicesKeywordId);
+        setCustomerUserId(parsedData.customerUserId);
+        setIdfv(parsedData.idfv);
+      } else {
+        await fetchIdfa();
+        //await requestOneSignallFoo();
+        await performAppsFlyerOperations();
+        await getUidApps();
+        await fetchAdServicesToken(); // Вставка функції для отримання токену
+        await fetchAdServicesAttributionData(); // Вставка функції для отримання даних
+
+        onInstallConversionDataCanceller();
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  ///////// Ad Attribution
+  //fetching AdServices token
+  const fetchAdServicesToken = async () => {
+    try {
+      const token = await AppleAdsAttribution.getAdServicesAttributionToken();
+      setAdServicesToken(token);
+      //Alert.alert('token', adServicesToken);
+    } catch (error) {
+      await fetchAdServicesToken();
+      //console.error('Помилка при отриманні AdServices токену:', error.message);
+    }
+  };
+
+  //fetching AdServices data
+  const fetchAdServicesAttributionData = async () => {
+    try {
+      const data = await AppleAdsAttribution.getAdServicesAttributionData();
+      const attributionValue = data.attribution ? '1' : '0';
+      setAdServicesAtribution(attributionValue);
+      setAdServicesKeywordId(data.keywordId);
+      //Alert.alert('data', data)
+    } catch (error) {
+      console.error('Помилка при отриманні даних AdServices:', error.message);
+    }
+  };
+
+  ///////// AppsFlyer
+  // 1ST FUNCTION - Ініціалізація AppsFlyer
+  const performAppsFlyerOperations = async () => {
+    try {
+      await new Promise((resolve, reject) => {
+        appsFlyer.initSdk(
+          {
+            devKey: 'XFmBDwMitGREaZSaboCCRR',
+            appId: '6737474419',
+            isDebug: true,
+            onInstallConversionDataListener: true,
+            onDeepLinkListener: true,
+            timeToWaitForATTUserAuthorization: 10,
+          },
+          resolve,
+          reject,
+        );
+      });
+      console.log('App.js AppsFlyer ініціалізовано успішно');
+      // Отримуємо idfv та встановлюємо його як customerUserID
+      const uniqueId = await DeviceInfo.getUniqueId();
+      setIdfv(uniqueId); // Зберігаємо idfv у стейті
+
+      appsFlyer.setCustomerUserId(uniqueId, res => {
+        console.log('Customer User ID встановлено успішно:', uniqueId);
+        setCustomerUserId(uniqueId); // Зберігаємо customerUserID у стейті
+      });
+    } catch (error) {
+      console.log(
+        'App.js Помилка під час виконання операцій AppsFlyer:',
+        error,
+      );
+    }
+  };
+
+  // 2ND FUNCTION - Ottrimannya UID AppsFlyer
+  const getUidApps = async () => {
+    try {
+      const appsFlyerUID = await new Promise((resolve, reject) => {
+        appsFlyer.getAppsFlyerUID((err, uid) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(uid);
+          }
+        });
+      });
+      //console.log('on getAppsFlyerUID: ' + appsFlyerUID);
+      //Alert.alert('appsFlyerUID', appsFlyerUID);
+      setAppsUid(appsFlyerUID);
+    } catch (error) {
+      //console.error(error);
+    }
+  };
+
+  // 3RD FUNCTION - Отримання найменування AppsFlyer
+  const onInstallConversionDataCanceller = appsFlyer.onInstallConversionData(
+    res => {
+      try {
+        const isFirstLaunch = JSON.parse(res.data.is_first_launch);
+        if (isFirstLaunch === true) {
+          if (res.data.af_status === 'Non-organic') {
+            const media_source = res.data.media_source;
+            console.log('App.js res.data==>', res.data);
+
+            const {campaign, pid, af_adset, af_ad, af_os} = res.data;
+            setSab1(campaign);
+            setPid(pid);
+          } else if (res.data.af_status === 'Organic') {
+            console.log('App.js res.data==>', res.data);
+            const {af_status} = res.data;
+            console.log('This is first launch and a Organic Install');
+            setSab1(af_status);
+          }
+        } else {
+          console.log('This is not first launch');
+        }
+      } catch (error) {
+        console.log('Error processing install conversion data:', error);
+      }
+    },
+  );
+
+  ///////// OneSignall
+  // 2abd1200-4d47-47c8-bd82-f9706b87ecf2
+
+  ///////// IDFA
+  const fetchIdfa = async () => {
+    try {
+      const res = await ReactNativeIdfaAaid.getAdvertisingInfo();
+      if (!res.isAdTrackingLimited) {
+        setIdfa(res.id);
+        //console.log('setIdfa(res.id);');
+      } else {
+        //console.log('Ad tracking is limited');
+        setIdfa(true); //true
+        //setIdfa(null);
+        fetchIdfa();
+        //Alert.alert('idfa', idfa);
+      }
+    } catch (err) {
+      //console.log('err', err);
+      setIdfa(null);
+      await fetchIdfa(); //???
+    }
+  };
 
   ///////// Route useEff
   // brilliant-magnificent-exhilaration.space
   useEffect(() => {
     const checkUrl = `https://reactnative.dev/`;
 
-    const targetData = new Date('2024-11-06T10:00:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2024-11-07T10:00:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
--- src/screens/PolishCraftHitOrigenProdactScreen.js
+++ src/screens/PolishCraftHitOrigenProdactScreen.js
@@ -1,11 +1,373 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
+import {
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Alert,
+  Linking,
+  Image,
+  Text,
+} from 'react-native';
+import {WebView} from 'react-native-webview';
+import DeviceInfo from 'react-native-device-info';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const PolishCraftHitOrigenProdactScreen = () => {
+const PolishCraftHitOrigenProdactScreen = ({navigation, route}) => {
+  //const [idfa, setIdfa] = useState(route.params?.idfa);
+  //console.log('route', route);
+  //const [uid, setUid] = useState(route.params?.uid);
+  //const [sab, setSab] = useState(route.params?.sab1);
+  //const [pid, setPid] = useState(route.params?.pid);
+  //const [adToken, setAdToken] = useState(route.params?.adToken);
+  //const [adAtribution, setAdAtribution] = useState(route.params?.adAtribution);
+  //const [adKeywordId, setAdKeywordId] = useState(route.params?.adKeywordId);
+  //const [idfv, setIdfv] = useState(route.params?.idfv);
+  ////console.log('idfvInProductScr============================>', idfv);
+  //const [customerUserId, setCustomerUserId] = useState(
+  //  route.params?.customerUserId,
+  //);
+
+  const refWebview = useRef(null);
+
+  const customSchemes = [
+    'scotiabank',
+    'bmoolbb',
+    'cibcbanking',
+    'conexus',
+    'rbcmobile',
+    'pcfbanking',
+    'tdct',
+    'blank',
+    'wise',
+    'https://app.rastpay.com/payment/',
+    'googlepay://',
+    'applepay://',
+  ];
+  {
+    /** 
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [
+    idfa,
+    uid,
+    sab,
+    pid,
+    adToken,
+    adAtribution,
+    adKeywordId,
+    idfv,
+    customerUserId,
+  ]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+        uid,
+        sab,
+        pid,
+        adToken,
+        adAtribution,
+        adKeywordId,
+        idfv,
+        customerUserId,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('SecretTreasureProdactScreen', jsonData);
+      console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem(
+        'SecretTreasureProdactScreen',
+      );
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('parsedData==>', parsedData);
+        setIdfa(parsedData.idfa);
+        setUid(parsedData.uid);
+        setSab(parsedData.sab);
+        setPid(parsedData.pid);
+        setAdToken(parsedData.adToken);
+        setAdAtribution(parsedData.adAtribution);
+        setAdKeywordId(parsedData.adKeywordId);
+        setCustomerUserId(parsedData.customerUserId);
+        setIdfv(parsedData.idfv);
+      } else {
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };*/
+  }
+
+  // кастомний юзерагент
+  const deviceInfo = {
+    deviceBrand: DeviceInfo.getBrand(),
+    deviceId: DeviceInfo.getDeviceId(),
+    deviceModel: DeviceInfo.getModel(),
+    deviceSystemName: DeviceInfo.getSystemName(),
+    deviceSystemVersion: DeviceInfo.getSystemVersion(),
+  };
+
+  //////////////////////////////
+  //let baseUrl = `https://marvelous-grand-joy.space/x3LZhrRP?advertising_id=${idfa}&uid=${uid}&adAtribution=${adAtribution}&adKeywordId=${adKeywordId}&customer_user_id=${customerUserId}&idfv=${idfv}`;
+  //let sabParts = sab ? sab.split('_') : [];
+  //let additionalParams = sabParts
+  //  .map((part, index) => `sub_id_${index + 1}=${part}`)
+  //  .join('&'); //
+  //const product = `${baseUrl}&${additionalParams}` + (pid ? `&pid=${pid}` : '');
+  //console.log('My product Url==>', product);
+  ////Alert.alert(product);
+
+  //const customUserAgent = `Mozilla/5.0 (${deviceInfo.deviceSystemName}; ${deviceInfo.deviceModel}) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.1`;
+  //const customUserAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0`;
+
+  const userAgent = `Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari/604.1`;
+  const customUserAgent = `${userAgent} Safari/604.1`;
+  console.log(customUserAgent);
+
+  useEffect(() => {
+    WebView.userAgent = customUserAgent;
+  }, []);
+  ///////////////////////////
+
+  //const [redirectUrl, setRedirectUrl] = useState(product);
+  const [checkNineUrl, setCheckNineUrl] = useState();
+  console.log('checkNineUrl====>', checkNineUrl);
+
+  const handleShouldStartLoad = event => {
+    const {url} = event;
+    //console.log('Should Start Load: ', url);
+    return true;
+  };
+
+  const handleNavigationStateChange = navState => {
+    const {url} = navState;
+    console.log('NavigationState: ', url);
+    //console.log('navState: ', navState);
+    if (
+      url.includes(
+        'https://api.paymentiq.io/paymentiq/api/piq-redirect-assistance',
+      )
+    ) {
+      setRedirectUrl(product);
+    } else if (url.includes('https://ninecasino')) {
+      setCheckNineUrl(product);
+    } else if (
+      url.includes('https://interac.express-connect.com/cpi?transaction=')
+    ) {
+      setRedirectUrl(product);
+    } else if (url.includes('about:blank') && checkNineUrl === product) {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      console.log('xxxx');
+    } else if (
+      url.includes('https://app.corzapay.com/payment/') &&
+      checkNineUrl === product
+    ) {
+      Linking.openURL(
+        `https://payment.paydmeth.com/en/cointy-white/payment/c13f7613-8ae7-48e0-8915-aa8187dd94ed`,
+      );
+      //refWebview.current.injectJavaScript(
+      //  `window.location.href = 'https://payment.paydmeth.com/en/cointy-white/payment/e82e61d0-1d94-4dcd-8b35-6122c69bae1a'`,
+      //);
+      console.log('WWWWW');
+    } else if (
+      url.includes('neteller') ||
+      url.includes('rapidtransfer') ||
+      url.includes('skrill') ||
+      (url.includes('paysafecard') && checkNineUrl === product)
+    ) {
+      //Linking.openURL(url);
+      //return false;
+      return; // Дозволити навігацію для цих URL-адрес
+    }
+  };
+
+  const onShouldStartLoadWithRequest = event => {
+    const {url} = event;
+    //console.log('onShouldStartLoadWithRequest========> ', url);
+
+    if (url.startsWith('mailto:')) {
+      Linking.openURL(url);
+      return false;
+    } else if (url.startsWith('itms-appss://')) {
+      Linking.openURL(url);
+      return false;
+    } else if (
+      url.includes('bitcoin') ||
+      url.includes('litecoin') ||
+      url.includes('dogecoin') ||
+      url.includes('tether') ||
+      url.includes('ethereum') ||
+      url.includes('bitcoincash')
+    ) {
+      return false;
+    } else if (
+      url.startsWith('https://m.facebook.com/') ||
+      url.startsWith('https://www.facebook.com/') ||
+      url.startsWith('https://www.instagram.com/') ||
+      url.startsWith('https://twitter.com/') ||
+      url.startsWith('https://www.whatsapp.com/') ||
+      url.startsWith('https://t.me/') ||
+      url.includes('https://web.telegram')
+    ) {
+      Linking.openURL(url);
+      return false;
+    } else if (url.includes('pay.skrill.com') && checkNineUrl === product) {
+      //console.log('Hello!!!!!!!!!!!!!!!!!!!!!');
+      Linking.openURL(url);
+      return false;
+    } else if (url.includes('applepay://') || url.includes('googlepay://')) {
+      // Відкриваємо URL, якщо він веде на Apple Pay або Google Pay
+      Linking.openURL(url);
+      return false;
+    } else if (
+      url.includes('app.rastpay.com/payment') &&
+      checkNineUrl === product
+    ) {
+      //console.log('Wise!');
+      Linking.openURL(
+        `https://openbanking.paysolo.net/session/38174d728a-730e664b72498a6f-GjwWW08AOP`,
+      );
+      return false;
+    } else if (url === 'https://jokabet.com/') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url === 'https://ninecasino.com/') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url === 'https://bdmbet.com/') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url === 'https://winspirit.app/?identifier=') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url.includes('https://rocketplay.com/api/payments')) {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url.includes('secure.livechatinc.com/customer/action/')) {
+      //refWebview?.current?.goBack();
+      return false;
+    } else {
+      const scheme = url.split(':')[0];
+      if (customSchemes.includes(scheme)) {
+        Linking.canOpenURL(url)
+          .then(canOpen => {
+            if (canOpen) {
+              Linking.openURL(url).catch(error => {
+                console.warn(`Unable to open URL: ${url}`, error);
+              });
+            } else {
+              Alert.alert(`The ${scheme} app is not installed on your device.`);
+            }
+          })
+          .catch(error => {
+            console.warn(`Error checking if URL can be opened: ${url}`, error);
+          });
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  //ф-ція для повернення назад
+  const goBackBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.goBack();
+    }
+  };
+
+  //ф-ція для оновлення сторінки
+  const reloadPageBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.reload();
+    }
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Polish Craft Hit Origen Product Screen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#191d24'}}>
+      <WebView
+        originWhitelist={[
+          '*',
+          'http://*',
+          'https://*',
+          'intent://*',
+          'tel:*',
+          'mailto:*',
+        ]}
+        //onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+        //onNavigationStateChange={handleNavigationStateChange}
+        source={{
+          uri: `https://reactnative.dev/`,
+        }}
+        textZoom={100}
+        allowsBackForwardNavigationGestures={true}
+        domStorageEnabled={true}
+        javaScriptEnabled={true}
+        allowsInlineMediaPlayback={true}
+        setSupportMultipleWindows={false}
+        mediaPlaybackRequiresUserAction={false}
+        allowFileAccess={true}
+        javaScriptCanOpenWindowsAutomatically={true}
+        style={{flex: 1}}
+        ref={refWebview}
+        userAgent={customUserAgent}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: -20,
+          paddingTop: 10,
+        }}>
+        {/**Btn back */}
+        <TouchableOpacity
+          style={{marginLeft: 40}}
+          onPress={() => {
+            goBackBtn();
+          }}>
+          <Image
+            style={{width: 30, height: 33}}
+            source={require('../assets/quiz/arrow77.png')}
+          />
+        </TouchableOpacity>
+
+        {/**Btn reload */}
+        <TouchableOpacity
+          style={{marginRight: 40}}
+          onPress={() => {
+            reloadPageBtn();
+          }}>
+          <Image
+            style={{width: 30, height: 30}}
+            source={require('../assets/quiz/redo77.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
